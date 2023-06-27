f3c55e201434c567c338da48324fe46a1496c075
Rafael Aranha <46253366+aranharafael@users.noreply.github.com>
2023-06-27 18:28:47 -0300

    Update orbit.py

--- orbit.py
+++ orbit.py
@@ -2,18 +2,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def f(t, x, y, vx, vy):
-    return vx
+def f_(t, x, y, vx, vy):
+    res = vx
+    return res
 
-def g(t, y, x, vy, vx):
-    return vy
+def g_(t, x, y, vx, vy):
+    res = vy
+    return res
 
-def i(t, x, y, vx, vy):
-    return (G*M*x)/(((x ** 2) + (y ** 2)) ** (3 / 2))
+def i_(t, x, y, vx, vy):
+    res = (-G*M*x)/(((x ** 2) + (y ** 2)) ** (3 / 2))
+    return res
 
-def j(t, y, x, vy, vx):
-    return (G*M*y)/(((x ** 2) + (y ** 2)) ** (3 / 2))
-
+def j_(t, x, y, vx, vy):
+    res = (-G*M*y)/(((x ** 2) + (y ** 2)) ** (3 / 2))
+    return res
 
 def RK4(f, g, i, j, x0, y0, vx0, vy0, t0, h, n):
     x = np.zeros(n)
@@ -26,29 +29,29 @@ def RK4(f, g, i, j, x0, y0, vx0, vy0, t0, h, n):
     vx[0] = vx0
     vy[0] = vy0
     t[0] = t0
-
+    print(x[0], y[0], vx[0], vy[0], t[0])
     for k in range(n-1):
-        kvx1 = h * i(t[k], x[k], y[k], vx[k], vy[k])
-        kvx2 = h * i(t[k] + h / 2, x[k] + kvx1 / 2, y[k] + kvx1 / 2, vx[k] + kvx1 / 2, vy[k] + kvx1 / 2)
-        kvx3 = h * i(t[k] + h / 2, x[k] + kvx2 / 2, y[k] + kvx2 / 2, vx[k] + kvx2 / 2, vy[k] + kvx2 / 2)
-        kvx4 = h * i(t[k] + h, x[k] + kvx3, y[k] + kvx3, vx[k] + kvx3 / 2, vy[k] + kvx3)
-
-        kvy1 = h * j(t[k], y[k], x[k], vy[k], vx[k])
-        kvy2 = h * j(t[k] + h / 2, y[k] + kvy1 / 2, x[k] + kvy1 / 2, vy[k] + kvy1 / 2, vx[k] + kvy1 / 2)
-        kvy3 = h * j(t[k] + h / 2, y[k] + kvy2 / 2, x[k] + kvy2 / 2, vy[k] + kvy2 / 2, vx[k] + kvy2 / 2)
-        kvy4 = h * j(t[k] + h, y[k] + kvy3, x[k] + kvy3, vy[k] + kvy3, vx[k] + kvy3)
-
         kx1 = h * f(t[k], x[k], y[k], vx[k], vy[k])
-        kx2 = h * f(t[k] + h / 2, x[k] + kx1 / 2, y[k] + kx1 / 2, vx[k] + kx1 / 2, vy[k] + kx1 / 2)
-        kx3 = h * f(t[k] + h / 2, x[k] + kx2 / 2, y[k] + kx2 / 2, vx[k] + kx2 / 2, vy[k] + kx2 / 2)
-        kx4 = h * f(t[k] + h, x[k] + kx3, y[k] + kx3, vx[k] + kx3/2, vy[k] + kx3)
+        ky1 = h * g(t[k], x[k], y[k], vx[k], vy[k])
+        kvx1 = h * i(t[k], x[k], y[k], vx[k], vy[k])
+        kvy1 = h * j(t[k], x[k], y[k], vx[k], vy[k])
+        
+        kx2 = h * f(t[k] + h / 2, x[k] + kx1 / 2, y[k] + ky1 / 2, vx[k] + kvx1 / 2, vy[k] + kvy1 / 2)
+        ky2 = h * g(t[k] + h / 2, x[k] + kx1 / 2, y[k] + ky1 / 2, vx[k] + kvx1 / 2, vy[k] + kvy1 / 2)
+        kvx2 = h * i(t[k] + h / 2, x[k] + kx1 / 2, y[k] + ky1 / 2, vx[k] + kvx1 / 2, vy[k] + kvy1 / 2)
+        kvy2 = h * j(t[k] + h / 2, x[k] + kx1 / 2, y[k] + ky1 / 2, vx[k] + kvx1 / 2, vy[k] + kvy1 / 2)
+        
+        kx3 = h * f(t[k] + h / 2, x[k] + kx2 / 2, y[k] + ky2 / 2, vx[k] + kvx2 / 2, vy[k] + kvy2 / 2)
+        ky3 = h * g(t[k] + h / 2, x[k] + kx2 / 2, y[k] + ky2 / 2, vx[k] + kvx2 / 2, vy[k] + kvy2 / 2)
+        kvx3 = h * i(t[k] + h / 2, x[k] + kx2 / 2, y[k] + ky2 / 2, vx[k] + kvx2 / 2, vy[k] + kvy2 / 2)
+        kvy3 = h * j(t[k] + h / 2, x[k] + kx2 / 2, y[k] + ky2 / 2, vx[k] + kvx2 / 2, vy[k] + kvy2 / 2)
+        
+        kx4  = h * f(t[k] + h, x[k] + kx3, y[k] + ky3, vx[k] + kvx3, vy[k] + kvy3)
+        ky4  = h * g(t[k] + h, x[k] + kx3, y[k] + ky3, vx[k] + kvx3, vy[k] + kvy3)
+        kvx4 = h * i(t[k] + h, x[k] + kx3, y[k] + ky3, vx[k] + kvx3, vy[k] + kvy3)
+        kvy4 = h * j(t[k] + h, x[k] + kx3, y[k] + ky3, vx[k] + kvx3, vy[k] + kvy3)
 
-        ky1 = h * g(t[k], y[k], x[k], vy[k], vx[k])
-        ky2 = h * g(t[k] + h / 2, y[k] + ky1 / 2, x[k] + ky1 / 2, vy[k] + ky1 / 2, vx[k] + ky1 / 2)
-        ky3 = h * g(t[k] + h / 2, y[k] + ky2 / 2, x[k] + ky2 / 2, vy[k] + ky2 / 2, vx[k] + ky2 / 2)
-        ky4 = h * g(t[k] + h, y[k] + ky3, x[k] + ky3, vy[k] + ky3, vx[k] +ky3)
-
-        x[k+1] = x[k] + (1/6)*(kx1 + 2*kx2 + 2*kx3 +kx4)
+        x[k+1] = x[k] + (1/6)*(kx1 + 2*kx2 + 2*kx3 + kx4)
         y[k+1] = y[k] + (1/6)*(ky1 + 2*ky2 + 2*ky3 + ky4)
         vx[k+1] = vx[k] + (1/6) * (kvx1 + 2 * kvx2 + 2 * kvx3 + kvx4)
         vy[k+1] = vy[k] + (1/6) * (kvy1 + 2 * kvy2 + 2 * kvy3 + kvy4)
@@ -57,25 +60,25 @@ def RK4(f, g, i, j, x0, y0, vx0, vy0, t0, h, n):
     return x, y, vx, vy, t
 
 
-x0 = 15
-y0 = 1
-vx0 = 1000
-vy0 = 1000
-t0 = 0
-h = 0.01
-n = 20
-G = 6.67408*(10^(-11))
-M = 2*(10^30)
+x0_ = 4*10**(12)
+y0_ = 0
+vx0_ = 0
+vy0_ = 500
+t0_ = 0
+h_ = 0.01
+n_ = 20
+G = 6.67*(10**(-11))
+M = 1.99*(10**30)
 
 
-x, y, vx, vy, t = RK4(f, g, i, j, x0, y0, vx0, vy0, t0, h, n)
+x, y, vx, vy, t = RK4(f_, g_, i_, j_, x0_, y0_, vx0_, vy0_, t0_, h_, n_)
 
-plt.plot(t,x)
-plt.title('x x t')
+# plt.figure()
+# plt.plot(t,x)
+# plt.title('x vs t')
+
 plt.figure()
-
 plt.plot(x,y)
-plt.title('órbita')
-plt.figure()
+plt.title('x vs y')
 
 plt.show()
