f060642b2576df8a66c8a9c92a0d8b3b5465dc10
mathnick <104511083+mathnick@users.noreply.github.com>
2024-05-25 19:34:46 -0300

    Update Lotka-Volterra_RK4.py

--- Lotka-Volterra_RK4.py
+++ Lotka-Volterra_RK4.py
@@ -17,45 +17,52 @@ def RK4(f, g, x0, y0, t0, h, n):
 
     for k in range(n-1):
         k1 = h*f(t[k], x[k], y[k])
-        k2 = h*f(t[k]+h/2, x[k] + k1/2, y[k] + k1/2)
-        k3 = h*f(t[k]+h/2, x[k] + k2/2, y[k] + k2/2)
-        k4 = h*f(t[k]+h, x[k] + k3, y[k] + k3)
         kk1 = h * g(t[k], y[k], x[k])
-        kk2 = h * g(t[k] + h / 2, y[k] + kk1 / 2, x[k] + kk1/2)
-        kk3 = h * g(t[k] + h / 2, y[k] + kk2 / 2, x[k] + kk2/2)
+
+        k2 = h*f(t[k]+h/2, x[k] + k1/2, y[k] + k1/2)
+        kk2 = h * g(t[k] + h / 2, y[k] + kk1 / 2, x[k] + kk1 / 2)
+
+        k3 = h*f(t[k]+h/2, x[k] + k2/2, y[k] + k2/2)
+        kk3 = h * g(t[k] + h / 2, y[k] + kk2 / 2, x[k] + kk2 / 2)
+
+        k4 = h*f(t[k]+h, x[k] + k3, y[k] + k3)
         kk4 = h * g(t[k] + h, y[k] + kk3, x[k] + kk3)
+
         x[k+1] = x[k] + (1/6)*(k1 + 2*k2 + 2*k3 +k4)
         y[k+1] = y[k] + (1/6)*(kk1 + 2*kk2 + 2*kk3 + kk4)
         t[k+1] = t[k] + h
+
         print(x[k+1],y[k+1],t[k+1])
     return x,y,t
 
 
 x0=1
-y0=1
+y0=0.5
 t0=0
-h=0.01
-n=2000
+h=0.0001
+n=400000
 a=1
-b=1
-c=1
-d=2
+b=0.5
+c=0.75
+d=0.5
 
 x, y, t = RK4(f, g, x0, y0, t0, h, n)
 
 
-plt.plot(t,x)
-plt.plot(t,y)
+plt.plot(t,x, label = 'presas')
+plt.plot(t,y, label = 'predadores')
 plt.title('Evolução das popoulções ao longo do tempo')
 plt.xlabel('tempo')
 plt.ylabel('população')
+plt.legend()
 plt.figure()
 
 
 plt.plot(x,y)
-plt.title('Relação entre as populções de lobos e coelhos')
-plt.xlabel('coelhos')
-plt.ylabel('lobos')
+plt.title('Relação entre as populções de presas e predadores')
+plt.xlabel('presas')
+plt.ylabel('predadores')
 plt.figure()
 
+plt.show()
 plt.show()
